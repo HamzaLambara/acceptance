2e6d7eaad7686eaad38c81588e65b84f3bdb1f6d
Hamza Lambara <lambara.hamza@gmail.com>
2023-12-08 19:07:42 +0100

    Ajouter tout

--- acceptance_test.sh
+++ acceptance_test.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-test $(curl localhost:8765/sum?a=20\&b=80) -eq 100
+response=$(curl -s -o /dev/null -w "%{http_code}" localhost:8765/sum?a=80\&b=20)
+
+if [ "$response" -eq 200 ]; then
+    echo "Test passed successfully."
+    exit 0
+else
+    echo "Test failed. HTTP response code: $response"
+    exit 1
+fi
+
