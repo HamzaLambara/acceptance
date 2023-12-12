e655ac56ab8d4f877b7d22e49866dd1217991a8b
Hamza Lambara <lambara.hamza@gmail.com>
2023-12-12 01:24:54 +0100

    Ajouter tout

--- acceptance_test.sh
+++ acceptance_test.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-response=$(curl localhost:8765/sum?a=80\&b=20)
-if [ $? -ne 0 ]; then
-    echo "Failed to connect to service"
-    exit 1
-fi
-
-test $response -eq 100
-
+test $(wget localhost:8765/sum?a=20\&b=80) -eq 100
