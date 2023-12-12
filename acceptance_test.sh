5ef926ebc7fed6c8e5a628f856cbfb59ba2a0db6
Hamza Lambara <lambara.hamza@gmail.com>
2023-12-12 01:31:36 +0100

    Ajouter tout

--- acceptance_test.sh
+++ acceptance_test.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-test $(wget localhost:8765/sum?a=20\&b=80) -eq 100
+
+# Run wget and capture the result in a variable
+result=$(wget -O - "localhost:8765/sum?a=20&b=80" 2>/dev/null)
+
+# Check if the result is numeric and perform the comparison
+if [[ "$result" =~ ^[0-9]+$ ]] && [ "$result" -eq 100 ]; then
+  echo "Test Passed!"
+else
+  echo "Test Failed!"
+  exit 1  # Exit with a non-zero status to indicate failure
+fi
+
